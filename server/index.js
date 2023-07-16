f7a81c6f1580763bf2504354a0b44ad95ae950a3
Aleesha2002 <aleesha.lalit@gmail.com>
2023-07-16 22:45:11 +0530

    updated cors

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "https://crowdfunding-complete-aeqp.vercel.app",
+    origin: "*",
   })
 );
 app.use(bodyParser.urlencoded({ extended: true }));
